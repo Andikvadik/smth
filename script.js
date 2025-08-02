a1c385d2ded5322c72985a3ef50c7b572f732a1b
Andikvadik <andriankaxuliganka@gmail.com>
2025-08-02 19:05:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ function chooseuserchoice(choice) {
   const result= document.getElementById("result");
   result.textContent='Можна було і покраще вибрати! ';
 }
+function chooseuserchoice(choice) {
+  const result = document.getElementById("result");
+  const img = document.getElementById("funnyImage");
+
+  result.textContent = `Ти обрав: "${choice}". Можна було і покраще вибрати!`;
+
+  img.classList.add('show-spin');
+}
+
